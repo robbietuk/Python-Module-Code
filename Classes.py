2de73d82991d2fe8e5843c96e55a3dc1fed24eab
Robert Twyman <robbietuk@gmail.com>
2018-10-17 17:32:23 +0100

    Added methods for testing if myroom is full or overful

--- Classes.py
+++ Classes.py
@@ -2,9 +2,24 @@
 
 
 class Room(object):
-    pass
+    def overful(self):
+        return len(self.occupants) > self.capacity
+    def full(self):
+        return len(self.occupants) == self.capacity
 
 
 myroom = Room()
-type(myroom)
+myroom.name = "Kitchen"
+myroom.capacity = 1
+myroom.occupants = ["Bob"]
+
+
+
+print(myroom.overful())
+print(myroom.full())
+
+
+
+
+
 
