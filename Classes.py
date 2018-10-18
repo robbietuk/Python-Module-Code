ff8168cc68eae6f2a58483bd97d304f6aa04a3d3
Robert Twyman <robbietuk@gmail.com>
2018-10-18 11:26:28 +0100

    Completed the maze model task with dictionaries

--- Classes.py
+++ Classes.py
@@ -1,26 +1,17 @@
-
-
-
 class Room(object):
     def __init__(self, name, exits, capacity, occupants=[]):
         self.name = name
         self.exits = exits
         self.occupants = occupants
         self.capacity = capacity
-    def overful(self):
+
+    def overfull(self):
         return len(self.occupants) > self.capacity
+
     def full(self):
         return len(self.occupants) == self.capacity
 
 
 Kitchen = Room("Kitchen", {'Garden': 'North'}, 3, ['Bob'])
 
-
-
 print(Kitchen.occupants)
-
-
-
-
-
-
